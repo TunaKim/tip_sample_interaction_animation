4ae2df6b9a666a073c205dc8e7b6e9be020cc635
김동원 <kdwon95@gmail.com>
2022-07-05 14:50:23 +0900

    change figure size

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-# importing all important librarie
+# importing all important libraries
 import matplotlib.animation
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -16,21 +16,23 @@ del data['index']
 #print(data)
 
 # plotting the graph
-fig = plt.figure()
+fig = plt.figure(figsize=(6,10))
 x1 = data.loc[:,'x']
 y1 = data.loc[:,'y']
 
 #plt.scatter(x = 'x', y = 'y', data = data)
 def a(i):
     fig.clear()
-    plt.scatter(x=x1[i], y=y1[i], s=250, marker='v')
-    plt.title('cantilever amp', fontsize=15)
-    plt.xlabel('position')
-    plt.ylabel('amplitude (nm)')
-    plt.xlim(-1,1)
+    plt.scatter(x=x1[i], y=y1[i], s=400, marker='v')
+    plt.title('cantilever amp', fontsize=30)
+    plt.ylabel('amplitude (nm)',fontsize=20)
+    plt.xlim(-0.5,0.5)
     plt.ylim(-10,10)
-    plt.hlines(-8+(i/100), -1, 1, color='gray', linestyle='solid', linewidth=3)
-    plt.text(0,-8+(i/100),'Sample surface',va='top',ha='center',fontsize=12)
+    plt.yticks(size=20)
+    plt.gca().axes.get_xaxis().set_visible(False)
+    plt.hlines(-8+(i/100), -0.5, 0.5, color='gray', linestyle='solid', linewidth=5)
+    plt.text(0,-8.2+(i/100),'Sample surface',va='top',ha='center',fontsize=20)
+    plt.tight_layout()
     return
 
 anima1 = FuncAnimation(fig, a, frames = len(data), interval=1)
